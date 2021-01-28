d8589361b4fa36981b07fe398967bb2f7356e53a
LalitTurbot <lalit@turbot.com>
2021-01-28 15:51:38 +0530

    update

--- gcp/plugin.go
+++ gcp/plugin.go
@@ -25,7 +25,7 @@ func Plugin(ctx context.Context) *plugin.Plugin {
 		},
 		TableMap: map[string]*plugin.Table{
 			"gcp_audit_policy":                    tableGcpAuditPolicy(ctx),
-			"gcp_cloudfunctions_function":          tableGcpCloudfunctionFunction(ctx),
+			"gcp_cloudfunctions_function":         tableGcpCloudfunctionFunction(ctx),
 			"gcp_compute_address":                 tableGcpComputeAddress(ctx),
 			"gcp_compute_global_address":          tableGcpComputeGlobalAddress(ctx),
 			"gcp_compute_global_forwarding_rule":  tableGcpComputeGlobalForwardingRule(ctx),
