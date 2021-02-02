0ff5701a985115906c1b84dd8e5f8c19cdb3f8cf
Lalit Bhardwaj <37527306+LalitTurbot@users.noreply.github.com>
2021-02-02 12:45:18 +0530

    Add gcp_compute_disk table. Closes #47 (#56)
    * Add gcp_compute_disk table. Closes #47
    
    * Add integration test. #47
    
    * Add docs for disk
    
    Co-authored-by: rajeshbal65 <rajesh.bal@turbot.com>

--- gcp/plugin.go
+++ gcp/plugin.go
@@ -27,6 +27,7 @@ func Plugin(ctx context.Context) *plugin.Plugin {
 			"gcp_audit_policy":                    tableGcpAuditPolicy(ctx),
 			"gcp_cloudfunctions_function":         tableGcpCloudfunctionFunction(ctx),
 			"gcp_compute_address":                 tableGcpComputeAddress(ctx),
+			"gcp_compute_disk":                    tableGcpComputeDisk(ctx),
 			"gcp_compute_firewall":                tableGcpComputeFirewall(ctx),
 			"gcp_compute_global_address":          tableGcpComputeGlobalAddress(ctx),
 			"gcp_compute_global_forwarding_rule":  tableGcpComputeGlobalForwardingRule(ctx),
