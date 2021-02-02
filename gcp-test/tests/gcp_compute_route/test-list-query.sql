5bd05329b374adda3b9e237ca27bed48fca9307b
Subhajit Mondal <38218418+Subhajit97@users.noreply.github.com>
2021-02-02 13:44:03 +0530

    Add gcp compute route table. Closes #49 (#50)
    * Add gcp compute route table. Closes #49
    * Update docs/tables/gcp_compute_route.md
    
    Co-authored-by: rajeshbal65 <rajesh.bal@turbot.com>
    Co-authored-by: Lalit Bhardwaj <37527306+LalitTurbot@users.noreply.github.com>

---
+++ gcp-test/tests/gcp_compute_route/test-list-query.sql
@@ -0,0 +1,3 @@
+select name, description
+from gcp.gcp_compute_route
+where title = '{{ resourceName }}'
---
+++ gcp-test/tests/gcp_compute_route/test-turbot-query.sql
@@ -0,0 +1,3 @@
+select title, akas
+from gcp.gcp_compute_route
+where name = '{{ resourceName }}'
