a9e50b3ac3d3d15594f31a203705617191488cfd
rajeshbal65 <rajesh.bal@turbot.com>
2021-02-04 15:53:57 +0530

    Add properties in integration test

--- gcp-test/tests/gcp_compute_snapshot/test-list-query.sql
+++ gcp-test/tests/gcp_compute_snapshot/test-list-query.sql
@@ -1,3 +1,3 @@
-select name, description, source_disk, disk_size_gb, self_link, project, title, akas
+select name, description, source_disk, disk_size_gb, self_link, project, title, akas, labels, storage_locations, tags
 from gcp.gcp_compute_snapshot
 where akas::text = '["{{ output.resource_aka.value }}"]'
