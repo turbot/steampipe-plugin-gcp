1be3c81085bb3af0d45e2b6539e4f093e4a7c8a8
LalitTurbot <lalit@turbot.com>
2021-02-09 23:04:30 +0530

    update project in test

--- gcp-test/tests/gcp_compute_target_vpn_gateway/variables.tf
+++ gcp-test/tests/gcp_compute_target_vpn_gateway/variables.tf
@@ -6,7 +6,7 @@ variable "resource_name" {
 
 variable "gcp_project" {
   type        = string
-  default     = "pikachu-aaa"
+  default     = "niteowl-aaa"
   description = "GCP project used for the test."
 }
 
