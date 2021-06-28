dbac951bcd69645ed703b9008cecc009802da181
rajeshbal65 <rajesh.bal@turbot.com>
2021-06-28 09:59:18 +0530

    wip

--- gcp/table_gcp_compute_metric_read_ops_hourly.go
+++ gcp/table_gcp_compute_metric_read_ops_hourly.go
@@ -11,13 +11,13 @@ import (
 
 //// TABLE DEFINITION
 
-func tableGcpComputeDiskMetricReadOpsHourly(_ context.Context) *plugin.Table {
+func tableGcpDiskMetricReadOpsHourly(_ context.Context) *plugin.Table {
 	return &plugin.Table{
-		Name:        "gcp_compute_disk_metric_read_ops_hourly",
+		Name:        "gcp_disk_metric_read_ops_hourly",
 		Description: "GCP SQL Database Daily connections",
 		List: &plugin.ListConfig{
 			ParentHydrate: listComputeInstances,
-			Hydrate:       listComputeDiskMetricReadOpsHourly,
+			Hydrate:       listDiskMetricReadOpsHourly,
 		},
 		Columns: monitoringMetricColumns([]*plugin.Column{
 			{
@@ -32,16 +32,10 @@ func tableGcpComputeDiskMetricReadOpsHourly(_ context.Context) *plugin.Table {
 
 //// LIST FUNCTION
 
-func listComputeDiskMetricReadOpsHourly(ctx context.Context, d *plugin.QueryData, h *plugin.HydrateData) (interface{}, error) {
-	databaseinfo := h.Item.(*compute.Instance)
+func listDiskMetricReadOpsHourly(ctx context.Context, d *plugin.QueryData, h *plugin.HydrateData) (interface{}, error) {
+	instanceInfo := h.Item.(*compute.Instance)
 
-	// Get project details
-	projectData, err := activeProject(ctx, d)
-	if err != nil {
-		return nil, err
-	}
-	project := projectData.Project
-	dimentionValue := "\"" + project + ":" + databaseinfo.Name + "\""
+	dimentionValue := "\"" + instanceInfo.Name + "\""
 
-	return listMonitorMetricStatistics(ctx, d, "HOURLY", "\"compute.googleapis.com/instance/disk/read_ops_count\"", "resource.label.database_id = ", dimentionValue, "")
+	return listMonitorMetricStatistics(ctx, d, "HOURLY", "\"compute.googleapis.com/instance/disk/read_ops_count\"", "resource.label.instance_name = ", dimentionValue, "")
 }
