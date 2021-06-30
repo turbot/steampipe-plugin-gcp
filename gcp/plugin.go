3710fd305011877776acbc374673e76e2c224687
Rajesh Bal <46851617+rajeshbal65@users.noreply.github.com>
2021-06-30 21:36:29 +0530

    Add table gcp_compute_machine_type. Closes #219 (#238)

--- gcp/plugin.go
+++ gcp/plugin.go
@@ -44,6 +44,7 @@ func Plugin(ctx context.Context) *plugin.Plugin {
 			"gcp_compute_image":                                       tableGcpComputeImage(ctx),
 			"gcp_compute_instance":                                    tableGcpComputeInstance(ctx),
 			"gcp_compute_instance_template":                           tableGcpComputeInstanceTemplate(ctx),
+			"gcp_compute_machine_type":                                tableGcpComputeMachineType(ctx),
 			"gcp_compute_network":                                     tableGcpComputeNetwork(ctx),
 			"gcp_compute_node_group":                                  tableGcpComputeNodeGroup(ctx),
 			"gcp_compute_node_template":                               tableGcpComputeNodeTemplate(ctx),
