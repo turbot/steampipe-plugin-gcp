2d3e25b09c9ca0abcbd12489e11dd28094729f41
Rajesh Bal <46851617+rajeshbal65@users.noreply.github.com>
2021-07-02 00:07:51 +0530

    Add table gcp_compute_disk_metric_read_ops_hourly closes #223 (#260)

--- gcp/plugin.go
+++ gcp/plugin.go
@@ -38,6 +38,7 @@ func Plugin(ctx context.Context) *plugin.Plugin {
 			"gcp_compute_backend_bucket":                              tableGcpComputeBackendBucket(ctx),
 			"gcp_compute_backend_service":                             tableGcpComputeBackendService(ctx),
 			"gcp_compute_disk":                                        tableGcpComputeDisk(ctx),
+			"gcp_compute_disk_metric_read_ops_hourly":                 tableGcpComputeDiskMetricReadOpsHourly(ctx),
 			"gcp_compute_firewall":                                    tableGcpComputeFirewall(ctx),
 			"gcp_compute_forwarding_rule":                             tableGcpComputeForwardingRule(ctx),
 			"gcp_compute_global_address":                              tableGcpComputeGlobalAddress(ctx),
