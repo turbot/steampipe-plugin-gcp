d0f96278cbd334a3ff88dd14f567f51029290b13
ParthaI <47887552+ParthaI@users.noreply.github.com>
2021-07-09 00:24:03 +0530

    Add table gcp_sql_database_instance_metric_connections closes #266 (#267)

--- gcp/plugin.go
+++ gcp/plugin.go
@@ -98,6 +98,7 @@ func Plugin(ctx context.Context) *plugin.Plugin {
 			"gcp_sql_backup":                                          tableGcpSQLBackup(ctx),
 			"gcp_sql_database":                                        tableGcpSQLDatabase(ctx),
 			"gcp_sql_database_instance":                               tableGcpSQLDatabaseInstance(ctx),
+			"gcp_sql_database_instance_metric_connections":            tableGcpSQLDatabaseInstanceConnectionsMetric(ctx),
 			"gcp_sql_database_instance_metric_connections_daily":      tableGcpSQLDatabaseInstanceConnectionsMetricDaily(ctx),
 			"gcp_sql_database_instance_metric_connections_hourly":     tableGcpSQLDatabaseInstanceConnectionsMetricHourly(ctx),
 			"gcp_sql_database_instance_metric_cpu_utilization":        tableGcpSQLDatabaseInstanceCpuUtilizationMetric(ctx),
