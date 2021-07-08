a84f461d4ee5ee3a41f7f4167be7b548e476669f
cbruno10 <cody@turbot.com>
2021-07-08 14:53:03 -0400

    Update plugin.go

--- gcp/plugin.go
+++ gcp/plugin.go
@@ -98,7 +98,6 @@ func Plugin(ctx context.Context) *plugin.Plugin {
 			"gcp_sql_backup":                                          tableGcpSQLBackup(ctx),
 			"gcp_sql_database":                                        tableGcpSQLDatabase(ctx),
 			"gcp_sql_database_instance":                               tableGcpSQLDatabaseInstance(ctx),
-			"gcp_sql_database_instance_connections_daily":             tableGcpSQLDatabaseInstanceConnectionsMetricDaily(ctx),
 			"gcp_sql_database_instance_metric_connections":            tableGcpSQLDatabaseInstanceConnectionsMetric(ctx),
 			"gcp_sql_database_instance_metric_connections_daily":      tableGcpSQLDatabaseInstanceConnectionsMetricDaily(ctx),
 			"gcp_sql_database_instance_metric_connections_hourly":     tableGcpSQLDatabaseInstanceConnectionsMetricHourly(ctx),
