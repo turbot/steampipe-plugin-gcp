c676c9f3a64c58cd2e0934e79102e6457593a173
karanpopat <karanpopat@gmail.com>
2022-03-16 16:47:45 +0530

    Update table_gcp_kubernetes_cluster.go
    Add columns autopilot_enabled in gcp_kubernetes_cluster table. Closes #343

--- gcp/table_gcp_kubernetes_cluster.go
+++ gcp/table_gcp_kubernetes_cluster.go
@@ -47,6 +47,12 @@ func tableGcpKubernetesCluster(ctx context.Context) *plugin.Table {
 				Type:        proto.ColumnType_STRING,
 			},
 			{
+				Name:        "autopilot_enabled",
+				Description: "Autopilot configuration for the cluster.",
+				Type:        proto.ColumnType_BOOL,
+				Transform:   transform.FromField("Autopilot.Enabled"),
+			},
+			{
 				Name:        "description",
 				Description: "An optional description of this cluster.",
 				Type:        proto.ColumnType_STRING,
@@ -200,11 +206,6 @@ func tableGcpKubernetesCluster(ctx context.Context) *plugin.Table {
 			{
 				Name:        "autoscaling",
 				Description: "Cluster-level autoscaling configuration.",
-				Type:        proto.ColumnType_JSON,
-			},
-			{
-				Name:        "autopilot",
-				Description: "Autopilot configuration for the cluster.",
 				Type:        proto.ColumnType_JSON,
 			},
 			{
