dda212c52c2d925595924df39bf7e5444018599e
ParthaI <partha@turbot.com>
2022-06-27 12:17:43 +0530

    Update

--- gcp/table_gcp_bigquery_dataset.go
+++ gcp/table_gcp_bigquery_dataset.go
@@ -24,7 +24,9 @@ func tableGcpBigQueryDataset(ctx context.Context) *plugin.Table {
 		},
 		List: &plugin.ListConfig{
 			Hydrate:           listBigQueryDatasets,
-			ShouldIgnoreError: isIgnorableError([]string{"403", "404"}),
+			IgnoreConfig: &plugin.IgnoreConfig{
+				ShouldIgnoreErrorFunc: isNotFoundError([]string{"403", "404"}),
+			},
 		},
 		Columns: []*plugin.Column{
 			{
