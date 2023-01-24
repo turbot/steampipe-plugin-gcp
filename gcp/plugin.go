4d92b9b792b6ab5ecfc3bd41e821ce85f3f99b75
ParthaI <partha@turbot.com>
2023-01-24 16:43:57 +0530

    DefaultGetConfig added back to plugin.go file

--- gcp/plugin.go
+++ gcp/plugin.go
@@ -20,6 +20,9 @@ func Plugin(ctx context.Context) *plugin.Plugin {
 	p := &plugin.Plugin{
 		Name:             pluginName,
 		DefaultTransform: transform.FromCamel(),
+		DefaultGetConfig: &plugin.GetConfig{
+			ShouldIgnoreError: isIgnorableError([]string{"404", "400"}),
+		},
 		// Default ignore config for the plugin
 		DefaultIgnoreConfig: &plugin.IgnoreConfig{
 			ShouldIgnoreErrorFunc: shouldIgnoreErrorPluginDefault(),
