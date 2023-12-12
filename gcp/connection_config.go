428e76aa7e404434c57c13450fd6fae5c1fea0fb
misraved <ved@turbot.com>
2023-12-12 13:13:22 +0530

    Recompile plugin with steampipe-plugin-sdk v5.8.0

--- gcp/connection_config.go
+++ gcp/connection_config.go
@@ -2,34 +2,14 @@ package gcp
 
 import (
 	"github.com/turbot/steampipe-plugin-sdk/v5/plugin"
-	"github.com/turbot/steampipe-plugin-sdk/v5/plugin/schema"
 )
 
 type gcpConfig struct {
-	Project                   *string  `cty:"project"`
-	Credentials               *string  `cty:"credentials"`
-	CredentialFile            *string  `cty:"credential_file"`
-	ImpersonateServiceAccount *string  `cty:"impersonate_service_account"`
-	IgnoreErrorCodes          []string `cty:"ignore_error_codes"`
-}
-
-var ConfigSchema = map[string]*schema.Attribute{
-	"project": {
-		Type: schema.TypeString,
-	},
-	"credentials": {
-		Type: schema.TypeString,
-	},
-	"credential_file": {
-		Type: schema.TypeString,
-	},
-	"impersonate_service_account": {
-		Type: schema.TypeString,
-	},
-	"ignore_error_codes": {
-		Type: schema.TypeList,
-		Elem: &schema.Attribute{Type: schema.TypeString},
-	},
+	Project                   *string  `hcl:"project"`
+	Credentials               *string  `hcl:"credentials"`
+	CredentialFile            *string  `hcl:"credential_file"`
+	ImpersonateServiceAccount *string  `hcl:"impersonate_service_account"`
+	IgnoreErrorCodes          []string `hcl:"ignore_error_codes,optional"`
 }
 
 func ConfigInstance() interface{} {
