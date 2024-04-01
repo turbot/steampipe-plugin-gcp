31f32f5d90183e367861459496397136792207fe
Priyanka Chatterjee <priyanka.chatterjee@turbot.com>
2024-04-01 21:52:46 +0530

    Add pagination

--- gcp/table_gcp_tag_binding.go
+++ gcp/table_gcp_tag_binding.go
@@ -3,8 +3,9 @@ package gcp
 import (
 	"context"
 
-	resourcemanagerpb "cloud.google.com/go/resourcemanager/apiv3/resourcemanagerpb"
 	"cloud.google.com/go/resourcemanager/apiv3"
+	resourcemanagerpb "cloud.google.com/go/resourcemanager/apiv3/resourcemanagerpb"
+	"github.com/turbot/go-kit/types"
 	"github.com/turbot/steampipe-plugin-sdk/v5/grpc/proto"
 	"github.com/turbot/steampipe-plugin-sdk/v5/plugin"
 	"github.com/turbot/steampipe-plugin-sdk/v5/plugin/transform"
@@ -50,8 +51,22 @@ func tableGcpTagBinding(ctx context.Context) *plugin.Table {
 //// LIST FUNCTION
 
 func listGcpTagBindings(ctx context.Context, d *plugin.QueryData, _ *plugin.HydrateData) (interface{}, error) {
-	// Create Service Connection
 	parent := d.EqualsQuals["parent"].GetStringValue()
+
+	if parent == "" {
+		return nil, nil
+	}
+
+	pageSize := types.Int64(300)
+
+	limit := d.QueryContext.Limit
+	if d.QueryContext.Limit != nil {
+		if *limit < *pageSize {
+			pageSize = limit
+		}
+	}
+
+	// Create Service Connection
 	client, err := resourcemanager.NewTagBindingsClient(ctx)
 	if err != nil {
 		return nil, err
@@ -59,7 +74,8 @@ func listGcpTagBindings(ctx context.Context, d *plugin.QueryData, _ *plugin.Hydr
 
 	// Construct the request
 	req := &resourcemanagerpb.ListTagBindingsRequest{
-		Parent: parent,
+		Parent:   parent,
+		PageSize: int32(*pageSize),
 	}
 
 	// Iterate through the results
