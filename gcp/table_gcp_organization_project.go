0a3d182eb482ba1078c3daedb83156f15481bf35
Patrick Decat <pdecat@gmail.com>
2024-10-01 16:26:12 +0200

    feat: add gcp_organization_project table

--- gcp/table_gcp_organization_project.go
+++ gcp/table_gcp_organization_project.go
@@ -2,22 +2,20 @@ package gcp
 
 import (
 	"context"
-	"strings"
 
 	"github.com/turbot/steampipe-plugin-sdk/v5/grpc/proto"
 	"github.com/turbot/steampipe-plugin-sdk/v5/plugin"
 	"github.com/turbot/steampipe-plugin-sdk/v5/plugin/transform"
-	"google.golang.org/api/cloudresourcemanager/v1"
 )
 
 //// TABLE DEFINITION
 
-func tableGcpProject(_ context.Context) *plugin.Table {
+func tableGcpOrganizationProject(_ context.Context) *plugin.Table {
 	return &plugin.Table{
-		Name:        "gcp_project",
-		Description: "GCP Project",
+		Name:        "gcp_organization_project",
+		Description: "GCP Organization Project",
 		List: &plugin.ListConfig{
-			Hydrate: listGCPProjects,
+			Hydrate: listGCPOrganizationProjects,
 		},
 		Columns: []*plugin.Column{
 			{
@@ -102,23 +100,15 @@ func tableGcpProject(_ context.Context) *plugin.Table {
 
 //// LIST FUNCTION
 
-func listGCPProjects(ctx context.Context, d *plugin.QueryData, h *plugin.HydrateData) (interface{}, error) {
+func listGCPOrganizationProjects(ctx context.Context, d *plugin.QueryData, h *plugin.HydrateData) (interface{}, error) {
 	// Create Service Connection
 	service, err := CloudResourceManagerService(ctx, d)
 	if err != nil {
 		return nil, err
 	}
 
-	// Get project details
-
-	projectId, err := getProject(ctx, d, h)
-	if err != nil {
-		return nil, err
-	}
-	project := projectId.(string)
-	plugin.Logger(ctx).Debug("gcp_project.listGCPProjects", "project_id", project)
-
-	resp, err := service.Projects.List().Filter("id=" + project).Do()
+	// List projects
+	resp, err := service.Projects.List().Do()
 	if err != nil {
 		return nil, err
 	}
@@ -128,67 +118,4 @@ func listGCPProjects(ctx context.Context, d *plugin.QueryData, h *plugin.Hydrate
 	}
 
 	return nil, nil
-}
-
-//// HYDRATE FUNCTIONS
-
-func getProjectAka(ctx context.Context, d *plugin.QueryData, h *plugin.HydrateData) (interface{}, error) {
-	// Get project details
-	project := h.Item.(*cloudresourcemanager.Project)
-
-	// Build resource aka
-	akas := []string{"gcp://cloudresourcemanager.googleapis.com/projects/" + project.ProjectId}
-
-	return akas, nil
-}
-
-func getProjectAccessApprovalSettings(ctx context.Context, d *plugin.QueryData, h *plugin.HydrateData) (interface{}, error) {
-	// Create Service Connection
-	service, err := AccessApprovalService(ctx, d)
-	if err != nil {
-		plugin.Logger(ctx).Error("gcp_project.getProjectAccessApprovalSettings", "connection_error", err)
-		return nil, err
-	}
-
-	// Get project details
-	projectId := h.Item.(*cloudresourcemanager.Project).ProjectId
-
-	resp, err := service.Projects.GetAccessApprovalSettings("projects/" + projectId + "/accessApprovalSettings").Do()
-	if err != nil {
-		if strings.Contains(err.Error(), "404") {
-			return nil, nil
-		}
-		plugin.Logger(ctx).Error("gcp_project.getProjectAccessApprovalSettings", "api_err", err)
-		return nil, err
-	}
-	return resp, nil
-}
-
-func getProjectAncestors(ctx context.Context, d *plugin.QueryData, h *plugin.HydrateData) (interface{}, error) {
-	// Create Service Connection
-	service, err := CloudResourceManagerService(ctx, d)
-	if err != nil {
-		plugin.Logger(ctx).Error("gcp_project.getProjectAncestors", "connection_error", err)
-		return nil, err
-	}
-
-	// Get project details
-	projectId := h.Item.(*cloudresourcemanager.Project).ProjectId
-
-	resp, err := service.Projects.GetAncestry(projectId, &cloudresourcemanager.GetAncestryRequest{}).Do()
-	if err != nil {
-		if strings.Contains(err.Error(), "404") {
-			return nil, nil
-		}
-		plugin.Logger(ctx).Error("gcp_project.getProjectAncestors", "api_err", err)
-		return nil, err
-	}
-	return resp.Ancestor, nil
-}
-
-func projectSelfLink(_ context.Context, d *transform.TransformData) (interface{}, error) {
-	data := d.HydrateItem.(*cloudresourcemanager.Project)
-	selfLink := "https://cloudresourcemanager.googleapis.com/v1/projects/" + data.Name
-
-	return selfLink, nil
 }
