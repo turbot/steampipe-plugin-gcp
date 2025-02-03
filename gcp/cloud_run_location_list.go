5482025eaca57c1cef244a985e9d6990a41e303f
ParthaI <partha@turbot.com>
2025-02-03 11:07:42 +0530

    Update the comment

--- gcp/cloud_run_location_list.go
+++ gcp/cloud_run_location_list.go
@@ -9,6 +9,7 @@ import (
 // Based on the available documentation, there is no indication that the Cloud Run Admin API v1 supports specific locations that v2 does not.
 // Both API versions are designed to operate across all regions where Cloud Run is available.
 // The primary differences between v1 and v2 pertain to API design and compatibility, rather than regional support.
+// Using the Cloud Run V1 API to list supported regions will not have any negative impact, even though the Cloud Run V2 API is used in the gcp_cloud_run_* tables for building the region matrix. The region data remains consistent across both API versions, ensuring accurate coverage without affecting functionality.
 
 // https://cloud.google.com/run/docs/locations
 // BuildCloudRunLocationList :: return a list of matrix items, one per region specified
